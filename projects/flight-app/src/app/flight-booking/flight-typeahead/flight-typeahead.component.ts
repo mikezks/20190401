71616a4b13f4732ad57168500466760670c1568f
mikezks <michael.zikes@intauria.com>
2019-04-02 11:45:05 +0200

    RxJS

--- projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { timer, Observable, Subscription, Subject } from 'rxjs';
-import { tap, take, takeUntil } from 'rxjs/operators';
+import {tap, take, takeUntil, switchMap, debounceTime, filter} from 'rxjs/operators';
+import {FormControl} from "@angular/forms";
+import {Flight} from "@flight-workspace/flight-api";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-flight-typeahead',
@@ -12,15 +15,49 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   timerSubscription: Subscription;
   destroy$ = new Subject<boolean>();
 
-  constructor() { }
+  control = new FormControl();
+  flights$: Observable<Flight[]>;
+  loading: boolean;
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.initTypeahead();
+    //this.rxjsDemo();
+  }
+
+  initTypeahead(): void {
+    this.flights$ =
+        this.control.valueChanges
+            .pipe(
+                debounceTime(300),
+                filter((value: string) => value.length > 2),
+                tap(() => this.loading = true),
+                switchMap(value => this.load(value)),
+                tap(() => this.loading = false)
+            );
+  }
+
+  load(from: string): Observable<Flight[]> {
+      const url = "http://www.angular.at/api/flight";
+
+      const params = new HttpParams()
+          .set('from', from);
+
+      const headers = new HttpHeaders()
+          .set('Accept', 'application/json');
+
+      return this.http.get<Flight[]>(url, {params, headers});
+  }
+
+  rxjsDemo(): void {
     this.timer$ = timer(0, 1000)
       .pipe(
         //take(3),
         takeUntil(this.destroy$),
         tap(value => console.log('operator', value))
-      );
+        );
 
     /* this.timerSubscription = this.timer$
       .subscribe(
